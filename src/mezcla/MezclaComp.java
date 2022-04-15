bb575922696862af30dff01e8af73c103dd5a951
LorenzoPorras <70544866+LorenzoPorras@users.noreply.github.com>
2022-04-14 19:49:14 -0500

    cosa 2

--- src/mezcla/MezclaComp.java
+++ src/mezcla/MezclaComp.java
@@ -16,7 +16,9 @@ public class MezclaComp {
 	private static File[] f = new File[N];
 
 	public static void main(String[] args) {
-		Comparable[] a = { new Persona1("Lorenzo", 2), new Persona1("Lorenzo", 1), new Persona1("Lorenzo", 8) };
+		Comparable[] a = { new Persona1("Lorenzo", 2), new Persona1("Lorenzo", 1), new Persona1("Lorenzo", 8),
+				new Persona1("Lorenzo", 2), new Persona1("Lorenzo", 1), new Persona1("Lorenzo", 2),
+				new Persona1("Lorenzo", 1) };
 
 		try {
 			creacion(a);
@@ -35,9 +37,30 @@ public class MezclaComp {
 
 		Comparable valores[] = new Comparable[N2];
 		PrintWriter salida[] = new PrintWriter[N];
+		BufferedReader[] lecturas1 = new BufferedReader[N2];
+		for (int i = 0; i < N2; i++) {
+			FileReader fr = new FileReader(f[i].getName());
+			lecturas1[i] = new BufferedReader(fr);
 
-		int t = 0;
+		}
+		boolean[] activos = new boolean[N2];
+		for (int i = 0; i < valores.length; i++) {
+			String a = lecturas1[i].readLine();
+			if (a != null) {
+				valores[i] = a;
+				activos[i] = true;
+
+			} else {
+				valores[i] = a;
+				activos[i] = false;
+			}
+		}
+		for (int i = 0; i < lecturas1.length; i++) {
+			lecturas1[i].close();
+		}
+		int t;
 		do {
+			t = 0;
 			int escritura = N2;
 			for (int i = N2; i < N; i++) {
 				FileWriter fichero;
@@ -47,57 +70,74 @@ public class MezclaComp {
 				salida[i] = new PrintWriter(f[i].getName());
 
 			}
-			boolean[] activos = new boolean[N2];
+		
 			BufferedReader[] lecturas = new BufferedReader[N2];
 			for (int i = 0; i < N2; i++) {
 				FileReader fr = new FileReader(f[i].getName());
 				lecturas[i] = new BufferedReader(fr);
 
 			}
-			for (int i = 0; i < valores.length; i++) {
-				String a = lecturas[i].readLine();
-				if (a != null) {
-					valores[i] = a;
-					activos[i] = true;
-
-				} else {
-					valores[i] = a;
-					activos[i] = false;
-				}
-			}
+	
 
 			while (finDeTramo(activos)) {
 				t++;
 
-				
-				
-				
-				
-				
-				
 				int dato = pos(valores, activos);
+				if (dato >= 0) {
+					Comparable sig = lecturas[dato].readLine();
 
-				salida[escritura].println(valores[dato]);
+					if (sig == null || valores[dato].compareTo(sig) > 0) {
+						salida[escritura].println(valores[dato]);
+						valores[dato] = sig;
+						activos[dato] = false;
 
-				Comparable sig = lecturas[dato].readLine();
-				valores[dato] = sig;
-			}
+					} else if (valores[dato].compareTo(sig) <= 0) {
+						salida[escritura].println(valores[dato]);
+						valores[dato] = sig;
+					}
+				}
 
-			for (int i = N2; i < salida.length; i++) {
-				salida[i].close();
 			}
-			for (int i = 0; i < lecturas.length; i++) {
-				lecturas[i].close();
-			}
-			if(verificar(valores))t=t;else; t=0;
+			if (t > 0) {
+				for (int i = N2; i < salida.length; i++) {
+					salida[i].close();
+				}
+				for (int i = 0; i < lecturas.length; i++) {
+					lecturas[i].close();
+				}
+
+				if (verificar(valores)) {
+					t = t;
+					for (int i = 0; i < activos.length; i++) {
+						if(valores[i]!=null)
+						activos[i] = true;
+					}
+					escritura = (escritura < N - 1) ? escritura + 1 : N2;
+
+				} else if (escritura == N2) {
+					for (int i = 0; i < N2; i++) {
+						File a;
+						a = f[i];
+						f[i] = f[i + N2];
+						f[i + N2] = a;
+					}
+				}
+			} else
+				t = 0;
 
 		} while (t > 0);
 
 	}
+
 	public static boolean verificar(Comparable[] a) {
-		int i=0;while(i<a.length && a[i]!=null)i++;
-		if(i<a.length)return true;else return false;
-		
+		int i = 0;
+		while (i < a.length && a[i] == null)
+			i++;
+		if (i < a.length)
+			return true;
+		else
+			return false;
+
 	}
 
 	public static boolean finDeTramo(boolean[] a) {
@@ -113,21 +153,24 @@ public class MezclaComp {
 	public static int pos(Comparable[] a, boolean[] b) {
 		int pos = 0;
 		int n = 0;
-		while (n < a.length && a[n] == null)
+		while (n < a.length  && b[n] != true)
 			n++;
-		Comparable anterior = a[n];
+		if (n < a.length) {
+			Comparable anterior = a[n];
 
-		for (int i = 0; i < a.length; i++) {
+			for (int i = 0; i < a.length; i++) {
 
-			if (a[i] != null) {
-				if (anterior.compareTo(a[i]) >= 0 && b[i] == true) {
-					anterior = a[i];
-					pos = i;
+				if (a[i] != null) {
+					if (anterior.compareTo(a[i]) >= 0 && b[i] == true) {
+						anterior = a[i];
+						pos = i;
+					}
 				}
-			}
 
-		}
-		return pos;
+			}
+			return pos;
+		} else
+			return -1;
 
 	}
 
